ca38a48b70001f8ea6e4b5109e90ea6ee73ebd9d
Luke Lin <lukelin1991@yahoo.com>
2019-11-28 00:48:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -27,5 +27,6 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  return director_data
+
+  return director_data[0][:worldwide_gross]
 end
