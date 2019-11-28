ba6de85f820bf910c7a145437fab38e7855525e6
Luke Lin <lukelin1991@yahoo.com>
2019-11-28 00:52:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -28,3 +28,13 @@ end
 # using director_data as input
 def gross_for_director(director_data)
 end
+
+{"Stephen Spielberg"=>1357566430, 
+"Russo Brothers"=>2281002470, 
+"James Cameron"=>2571020373, 
+"Spike Lee"=>256624217, 
+"Wachowski Siblings"=>806180282, 
+"Robert Zemeckis"=>1273838385, 
+"Quentin Tarantino"=>662738268, 
+"Martin Scorsese"=>636812242, 
+"Francis Ford Coppola"=>509719258}
