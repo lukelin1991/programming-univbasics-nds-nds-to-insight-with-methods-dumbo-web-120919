c2187cc0e05267db88b08536ecad1f17a4c9ae31
Luke Lin <lukelin1991@yahoo.com>
2019-11-28 00:53:07 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -27,7 +27,7 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  puts directors_totals(nds)
+  puts directors_totals(nds(director_data[:name]))
 end
 
 {"Stephen Spielberg"=>1357566430,
